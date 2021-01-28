f1c97aeed0c14778a55e10bab8919db142902007
aurelienshz <aurelien.schiltz@free.fr>
2021-01-28 01:37:28 +0100

    history.testnet.minepi.com

--- pinetwork/upload-s3cmd.sh
+++ pinetwork/upload-s3cmd.sh
@@ -10,12 +10,12 @@ if [[ "$TARGET" == *.well-known/stellar-history.json ]]; then
 
   # -c option, because for some weird reason, s3cmd looks for the config file in /root although it's running as the stellar user
   # Cache-Control:no-cache doesn't work with the DigitalOcean CDN. But max-age=0 does.
-  /s3cmd/s3cmd -c /home/stellar/.s3cfg put --acl-public --add-header="Cache-Control":"max-age=0" $SOURCE s3://testnet-history/$TARGET
+  /s3cmd/s3cmd -c /home/stellar/.s3cfg put --acl-public --add-header="Cache-Control":"max-age=0" $SOURCE s3://history.testnet.minepi.com/$TARGET
 else
   # Any other class of history file: we can cache for the bucket's default cache TTL.
 
   echo "upload-s3cmd: upload with default cache"
 
   # -c option, because for some weird reason, s3cmd looks for the config file in /root although it's running as the stellar user
-  /s3cmd/s3cmd -c /home/stellar/.s3cfg put --acl-public $SOURCE s3://testnet-history/$TARGET
+  /s3cmd/s3cmd -c /home/stellar/.s3cfg put --acl-public $SOURCE s3://history.testnet.minepi.com/$TARGET
 fi
